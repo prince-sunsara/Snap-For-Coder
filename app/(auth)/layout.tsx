eac98db7c8a95b8a860cc19ea356e6dc4da428c9
prince-sunsara <princesunasara977@gmail.com>
2024-06-14 11:20:26 +0530

    Auth functionality fronted work done, started backend with database setup

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,12 +4,14 @@ import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
-      <div className="bg-white rounded-lg shadow-md min-w-80 p-8">
-        <Link href={"/"} className="flex justify-center mb-4">
-          <Image src={"/logo.svg"} width={40} height={40} alt="Logo" />
-        </Link>
-        {children}
+    <div className="bg-gradient-to-r from-slate-500 to-yellow-100">
+      <div className="flex flex-col justify-center items-center min-h-screen bg-auth-layout">
+        <div className="bg-white rounded-lg shadow-md min-w-80 p-8">
+          <Link href={"/"} className="flex justify-center mb-4">
+            <Image src={"/logo.svg"} width={40} height={40} alt="Logo" />
+          </Link>
+          {children}
+        </div>
       </div>
     </div>
   );
---
+++ models/user-model.ts
@@ -0,0 +1,44 @@
+import { Document, Model, Schema, model, models } from "mongoose";
+
+export interface UserInterface {
+  username: String;
+  fullName: String;
+  email: String;
+  avatar?: string;
+}
+
+export interface UserDocument extends UserInterface, Document {
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const userSchema = new Schema<UserDocument>(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    avatar: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    // created At, updated at
+    timestamps: true,
+  }
+);
+
+const User: Model<UserDocument> = models?.User || model("User", userSchema);
+
+export default User;
