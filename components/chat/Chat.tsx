a74e16bd020c6accf49a9bab69fc18657268602a
prince-sunsara <princesunasara977@gmail.com>
2024-06-20 19:48:34 +0530

    completed chat component work to show details

--- components/chat/Chat.tsx
+++ components/chat/Chat.tsx
@@ -2,6 +2,12 @@ import Link from "next/link";
 import React from "react";
 import { Avatar, AvatarImage } from "../ui/avatar";
 import Image from "next/image";
+import { formateDate } from "@/lib/utils";
+import {
+  ImageMessageSvg,
+  TextMessageSent,
+  TextMessageSvgReceived,
+} from "../svgs/ChatSvg";
 
 type chatProps = {
   chat: any;
@@ -9,6 +15,58 @@ type chatProps = {
 
 const Chat = ({ chat }: chatProps) => {
   const userToChat = chat.participants[0];
+
+  const lastMessage = chat.lastMessage;
+  const lastMessageType = lastMessage?.messageType;
+  const formattedDate = lastMessage
+    ? formateDate(lastMessage?.createdAt!)
+    : formateDate(new Date());
+
+  const amIsener = lastMessage && lastMessage.sender._id !== userToChat?._id;
+  const isMsgOpened = lastMessage?.opened;
+
+  let messageStatus: string;
+  let iconComponent: JSX.Element;
+
+  if (amIsener) {
+    messageStatus = isMsgOpened ? "Opened" : "Sent";
+    iconComponent =
+      lastMessageType === "text" ? (
+        <TextMessageSent
+          className={
+            isMsgOpened ? "text-sigSnapChat" : "text-sigSnapChat fill-current"
+          }
+        />
+      ) : (
+        <ImageMessageSvg
+          className={
+            isMsgOpened ? "text-sigSnapImg" : "text-sigSnapImg fill-current"
+          }
+        />
+      );
+  } else {
+    if (!lastMessage) {
+      messageStatus = "Say Hi!";
+      iconComponent = <TextMessageSvgReceived className="fill-current" />;
+    } else {
+      messageStatus = isMsgOpened ? "Received" : "Showing Message";
+      iconComponent =
+        lastMessageType === "text" ? (
+          <TextMessageSvgReceived
+            className={
+              isMsgOpened ? "text-sigSnapChat" : "text-sigSnapChat fill-current"
+            }
+          />
+        ) : (
+          <ImageMessageSvg
+            className={
+              isMsgOpened ? "text-sigSnapImg" : "text-sigSnapImg fill-current"
+            }
+          />
+        );
+    }
+  }
+
   return (
     <Link href={`/chat/${userToChat?._id}`}>
       <li className="flex items-center p-2 bg-sigSurface hover:bg-sigBackgroundFeedHover cursor-pointer border-b border-b-sigColorBgBorder">
@@ -18,6 +76,10 @@ const Chat = ({ chat }: chatProps) => {
 
         <div className="ml-3">
           <p>{userToChat?.fullName}</p>
+          <p className="flex gap-1 text-xs text-gray-400">
+            {iconComponent}
+            {messageStatus} - {formattedDate}
+          </p>
         </div>
 
         <Image
